3aab78c248ee8a4fdf580d92df15393561598dac
SongLi Huang <zjuhsl@gmail.com>
2020-01-10 16:36:28 +0800

    fix

--- src/main/java/com/github/pister/tson/Tsons.java
+++ src/main/java/com/github/pister/tson/Tsons.java
@@ -6,6 +6,7 @@ import com.github.pister.tson.models.Item;
 import com.github.pister.tson.parse.Lexer;
 import com.github.pister.tson.parse.LexerReader;
 import com.github.pister.tson.parse.Parser;
+import com.github.pister.tson.utils.StringUtil;
 
 /**
  * Created by songlihuang on 2020/1/5.
@@ -15,6 +16,9 @@ public final class Tsons {
     private Tsons() {}
 
     public static Object decode(String text) {
+        if (StringUtil.isEmpty(text)) {
+            return null;
+        }
         Lexer lexer = new Lexer(new LexerReader(new FastStringReader(text)));
         Parser parser = new Parser(lexer);
         Item item = parser.parse();
@@ -22,6 +26,9 @@ public final class Tsons {
     }
 
     public static String encode(Object o) {
+        if (o == null) {
+            return null;
+        }
         ItemStringWriter itemStringWriter = new ItemStringWriter();
         Item item = Item.wrap(o);
         itemStringWriter.write(item);
